ed92ae050244adf85912107a07f2c90530a40794
michael-manders <mjmanders1@icloud.com>
2023-07-30 14:12:06 -0500

    testing deplyment

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## About
 
-This is my personal website designed to be hosted on github pages. It is built using Gatsby so I can learn React. It also hosts some projects I have that are not built using Gatsby, but are still moved to the build directory.
+This is my personal website designed to be hosted on GitHub pages. It is built using Gatsby so I can learn React. It also hosts some projects I have that are not built using Gatsby, but are still moved to the build directory.
 
 ## How to set up
 
@@ -15,3 +15,7 @@ This is my personal website designed to be hosted on github pages. It is built u
 1. cd into the /src folder and run `npm install -i`
 2. cd back into the parent directory
 3. run `compile.py`
+
+## Deployment
+
+This GitHub repo has 2 branches (main and gh-pages). I push all the code to the main branch so it can be viewed here. I push just the code from the build directory to the gh-pages using the command: `git subtree push --prefix build origin gh-pages`. I have switched the branch GitHub pages deploys from to the gh-pages branch.
--- compile.py
+++ compile.py
@@ -1,13 +1,17 @@
 import subprocess
 import shutil
+import os
 
 
 def compile():
     # compile the gatsby code
     subprocess.call('cd src && npm run build', shell=True)
     print('Gatsby code compiled successfully!')
-    # cut the files from the public folder and paste them in the ./build folder
-    shutil.rmtree('./build')
+    try:
+        shutil.rmtree('./build')
+        print('Build folder removed successfully!')
+    except:
+        pass
     shutil.copytree('./src/public', './build')
     print('Files copied successfully!')
     # remove the public folder
@@ -20,5 +24,23 @@ def compile():
     print('Compilation completed successfully!')
 
 
+def deploy(message):
+    print("Deploying the build folder to gh-pages...")
+    # deploy the build folder to gh-pages
+    subprocess.call(
+        'git subtree push --prefix build origin gh-pages', shell=True)
+    print('Build folder deployed successfully!')
+
+    print("Deploying the src folder to master...")
+    # deploy the root folder to master
+    subprocess.call('git add .', shell=True)
+    subprocess.call(f'git commit -m "{message}"', shell=True)
+    subprocess.call('git push origin master', shell=True)
+
+    print('Deployment completed successfully!')
+
+
 if __name__ == '__main__':
     compile()
+    if input("Would you like to deply? (y/n): ").lower() == "y":
+        deploy(input("Enter the commit message: "))
