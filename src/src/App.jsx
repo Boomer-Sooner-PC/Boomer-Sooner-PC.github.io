55b612261389d165bc541a1171f19c156453ef87
michael-manders <mjmanders1@icloud.com>
2023-08-02 11:43:11 -0500

    new build

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -1,6 +1,5 @@
 import "./css/App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "pages/Home";
 import About from "pages/About";
 import Astronomy from "pages/Astronomy";
@@ -8,12 +7,12 @@ import Astronomy from "pages/Astronomy";
 function App() {
     return (
         <Router>
-            <Switch>
+            <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/astronomy" element={<Astronomy />} />
                 <Route path="*" element={<h1>404</h1>} />
-            </Switch>
+            </Routes>
         </Router>
     );
 }
