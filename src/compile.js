e4d96af463dcf55c11fe366c687c4a3549ca58e2
Michael Manders <MJManders1@icloud.com>
2023-08-21 21:57:10 -0500

    projects page addition

--- src/compile.js
+++ src/compile.js
@@ -3,14 +3,33 @@ const fs = require("fs");
 let projectsPath = "../projects/";
 let projects = fs.readdirSync(projectsPath);
 
+let photoIndex = 0;
+
 let projectsData = {};
 for (let project of projects) {
     let path = projectsPath + project + "/";
+
+    // read the data from data.json inside the folder
+    let fileData = JSON.parse(fs.readFileSync(path + "data.json", "utf8"));
+    // images in folder
+    let images = fs.readdirSync(path + "images/");
     let data = {
         name: project,
         build: fs.existsSync(path + "page.html"),
         about: fs.readFileSync(path + "README.md", "utf8"),
+        github: fileData.github,
+        images: [],
     };
+
+    for (let image of images) {
+        data.images.push(`img${photoIndex}.png`);
+        fs.copyFileSync(
+            path + "images/" + image,
+            `src/images/projectImages/img${photoIndex}.png`
+        );
+        photoIndex++;
+    }
+
     projectsData[project] = data;
 }
 
@@ -19,3 +38,17 @@ fs.writeFileSync(
     "src/data/projects.json",
     JSON.stringify(projectsData, null, 4)
 );
+
+let imageArray = [];
+for (let i = 0; i < photoIndex; i++) {
+    imageArray.push(`img${i}.png`);
+}
+
+let projectJsx = fs.readFileSync("src/pages/Projects.jsx", "utf8");
+// replace everything from let imageFileNames = [*]; with the imageArray
+projectJsx = projectJsx.replace(
+    /let imageFileNames = \[.*\];/g,
+    `let imageFileNames = ${JSON.stringify(imageArray)};`
+);
+
+fs.writeFileSync("src/pages/Projects.jsx", projectJsx);
--- src/src/pages/Projects.jsx
+++ src/src/pages/Projects.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import Navbar from "components/NavBar";
 import Background from "components/Background";
+import MarkdownIt from "markdown-it";
 
 import projectsData from "data/projects.json";
 import "../css/Projects.css";
 
 import dropdownImg from "../images/dropdown.png";
+import githubImg from "../images/github.png";
 
-import MarkdownIt from "markdown-it";
+let imageFileNames = ["img0.png","img1.png","img2.png","img3.png"];
+
+function importAll(r) {
+    return r.keys().map(r);
+}
+const images = importAll(
+    require.context("../images/projectImages", false, /\.(png|jpe?g|svg)$/)
+);
 
 export default class Projects extends React.Component {
+    constructor(props) {
+        super(props);
+        this.projectsGalleryStates = {};
+    }
+
     componentDidMount() {
         const md = new MarkdownIt();
 
@@ -18,6 +32,7 @@ export default class Projects extends React.Component {
 
             const projectDiv = document.createElement("div");
             projectDiv.classList.add("project");
+            projectDiv.classList.add(projectName);
 
             const projectHeader = document.createElement("div");
             projectHeader.className = "project-header";
@@ -42,6 +57,17 @@ export default class Projects extends React.Component {
                 projectLink.href = link;
                 projectLink.innerText = "View Project";
                 projectHeader.appendChild(projectLink);
+            }
+
+            if (project.github) {
+                const githubImgElm = document.createElement("img");
+                githubImgElm.className = "github-img";
+                githubImgElm.src = githubImg;
+                projectHeader.appendChild(githubImgElm);
+
+                githubImgElm.onclick = () => {
+                    window.open(project.github, "_blank");
+                };
             }
 
             const projectDropdown = document.createElement("img");
@@ -72,8 +98,68 @@ export default class Projects extends React.Component {
                     state === "none" ? "rotate(180deg)" : "rotate(0deg)";
             };
 
+            // add image gallery
+            const projectGallery = document.createElement("div");
+            projectGallery.className = "project-gallery";
+
+            const rightArrow = document.createElement("img");
+            rightArrow.className = "arrow right";
+            rightArrow.src = dropdownImg;
+
+            const leftArrow = document.createElement("img");
+            leftArrow.className = "arrow left";
+            leftArrow.src = dropdownImg;
+
+            projectGallery.appendChild(leftArrow);
+            projectGallery.appendChild(rightArrow);
+
+            // create another div for the image so it can be centered
+            const imageContainer = document.createElement("div");
+            imageContainer.className = "image-container";
+
+            const projectImage = document.createElement("img");
+            projectImage.id = `${projectName}-image`;
+            projectImage.className = "project-image";
+            projectImage.src =
+                images[imageFileNames.indexOf(project.images[0])];
+            imageContainer.appendChild(projectImage);
+
+            projectGallery.appendChild(imageContainer);
+            projectDescription.appendChild(projectGallery);
+            this.projectsGalleryStates[projectName] = {
+                index: 0,
+                length: project.images.length,
+            };
+
+            leftArrow.onclick = () => {
+                this.updateImageGallery(projectName, "left", project.images);
+            };
+            rightArrow.onclick = () => {
+                this.updateImageGallery(projectName, "right", project.images);
+            };
+
             document.querySelector(".projects").appendChild(projectDiv);
         }
+    }
+
+    updateImageGallery(projectName, direction, projectImages) {
+        const state = this.projectsGalleryStates[projectName];
+
+        if (direction === "left") {
+            state.index--;
+            if (state.index < 0) {
+                state.index = state.length - 1;
+            }
+        } else {
+            state.index++;
+            if (state.index >= state.length) {
+                state.index = 0;
+            }
+        }
+
+        const projectImage = document.querySelector(`#${projectName}-image`);
+        projectImage.src =
+            images[imageFileNames.indexOf(projectImages[state.index])];
     }
 
     render() {
